a9306b9f3d7828165d9d1193c37c66f1871b9d15
Jérémy Huard <63874667+ChezJe@users.noreply.github.com>
2026-07-16 15:39:37 +0200

    Added buildfile and export to MD

---
+++ buildfile.m
@@ -0,0 +1,21 @@
+function plan = buildfile
+import matlab.buildtool.tasks.*
+
+plan = buildplan(localfunctions);
+
+plan("clean") = CleanTask;
+plan("check") = CodeIssuesTask;
+
+proj = currentProject;
+plan("exportMDfile").Inputs = ...
+    fullfile(proj.RootFolder,"Feature_Ranking_script.m");
+plan("exportMDfile").Dependencies = "check";
+
+plan.DefaultTasks = "exportMDfile";
+end
+
+
+function exportMDfileTask(context)
+filenames = context.Task.Inputs.paths;
+export(filenames{:}, Format="markdown", EmbedImages=true, AcceptHTML=true);
+end
